4d5986a55574d7646f3eabb6c57e4ff06376e0c8
VaubanParty <yassine.badache@gmail.com>
2021-06-15 16:47:15 +0200

    Blank page with hard-coded, incomplete menu

---
+++ front/scripts/index.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Poulet coco',
+        description: 'Le meilleur poulet de l\'Ouest francophone',
+        ingredients: [
+            { text: 'Poulet' },
+            { text: 'Coco' }
+        ]
+    }
+});
